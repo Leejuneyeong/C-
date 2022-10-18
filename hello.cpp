24ad53a5350179ecf2dae98dedd492636ceee3a1
juneyeong Lee <48881531+Leejuneyeong@users.noreply.github.com>
2022-10-18 11:44:06 +0900

    입출력,반복문,조건문

--- hello.cpp
+++ hello.cpp
@@ -30,5 +30,50 @@ int main() {
 
 	std::cout << " while문 합 : " << sum2 << std::endl;
 
+	int lucky_number = 3;
+	std::cout << "내 비밀 수를 맞추어 보세요~" << std::endl;
+
+	int user_input;
+
+	while (1) {
+		std::cout << "입력 : ";
+		std::cin >> user_input;
+		if (lucky_number == user_input) {
+			std::cout << "맞추셨습니다~~" << std::endl;
+			break;
+		}
+		else {
+			std::cout << "다시 생각해보세요~" << std::endl;
+		}
+	}
+
+	// switch 문 이용하기
+	using std::cout;
+	using std::endl;
+	using std::cin;
+
+	int user_input2;
+	cout << "저의 정보를 표시해줍니다" << endl;
+	cout << "1. 이름 " << endl;
+	cout << "2. 나이 " << endl;
+	cout << "3. 성별 " << endl;
+
+	cin >> user_input2;
+
+	switch (user_input2) {
+		case 1:
+			cout << "Psi ! " << endl;
+			break;
+		case 2:
+			cout << "99 살" << endl;
+			break;
+		case 3:
+			cout << "남자" << endl;
+			break;
+		default:
+			cout << "궁금한게 없군요~" << endl;
+			break;
+		}
+
 	return 0;
 }
